db1670e1bc3750a4d0395b5665e935f67481d8f1
Veeryadavp <raghu84yadav@gmail.com>
2024-07-29 19:20:43 +0100

    Update EqualExpense.java

--- src/EqualExpense.java
+++ src/EqualExpense.java
@@ -1,4 +1,18 @@
 public class EqualExpense extends Expense
 {
-
+ private list<User> users;
+     public EqualExpense(String expenseID,User paidBy, double totalAmount, list<User> users)
+     {
+         super(expenseID, paidBy, amount, Expensetype.EQUAL)
+         this.users=users;
+     }
+     
+     public void splitExpense()
+     {
+         double AmtPerUser=math.round((getTotalAmount()/users.size()*100.0)/100.0);
+         for(User user:users)
+         {
+             System.out.println(user.getName()+" owes "+AmtPerUser);
+         }
+     }
 }
