7b901d0c43e35b418aaaaeb1461876874dd23a9d
Veeryadavp <raghu84yadav@gmail.com>
2024-07-29 19:19:54 +0100

    Update PercentExpense.java

--- src/PercentExpense.java
+++ src/PercentExpense.java
@@ -1,4 +1,19 @@
 public class PercentExpense
 {
-
+private map<User, Double> userShares;
+    
+    public PercentExpense(String ExpenseID, User paidBy, double totalAmount, map<User, Double> userShares)
+    {
+        super(ExpenseID, paidBy, totalAmount, Expensetype.PERCENT);
+        this.userShares=userShares;
+    }
+    
+    public void splitExpense()
+    {
+        double totalPercentage=userShares.values().stream().mapToDouble(Double::doubleValue).sum();
+        if(totalPercentage!=100.0)
+    }
+    throw new IllegalArgumentException("Total percentage does not equal 100.");
+    }
+    userShares.forEach()
 }
