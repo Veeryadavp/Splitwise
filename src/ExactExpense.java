379c0aba0b04e9db0b148a1518ae80e9e7daa57f
Veeryadavp <raghu84yadav@gmail.com>
2024-07-29 19:20:16 +0100

    Update ExactExpense.java

--- src/ExactExpense.java
+++ src/ExactExpense.java
@@ -1,4 +1,21 @@
 public class ExactExpense
 {
-
+private map(User, Double) userShares;
+    
+    public ExactExpense(String expensID, User paidBy, double totalAmount, map<User, Double userShares)
+    {
+    super(expenseID, paidBy, totalAmount, Expensetype.EXACT);
+    this.userShares=userShares;
+    }
+ public void splitExpense()
+ {
+     double totalShares=userShares.values().stream().mapToDouble(Doule::doubleValue).sum();
+     if(totalShares!=getTotalAmount)
+     {
+         throw new IllegalArgumentException("Total shres do not equal the total amount.");
+     }
+     {
+         userShares.forEach((User, amount)->System.out.println(User.getUserName()+" owes "+amount));
+     }
+ }
 }
