2f0a6c3f3fe8c37fd3cbceca3b733a98e30496b3
rp22acg <raghu84yadav@gmail.com>
2024-07-30 21:44:11 +0100

    Final corrections done.

--- src/ExactExpense.java
+++ src/ExactExpense.java
@@ -2,22 +2,22 @@ import java.util.Map;
 
 public class ExactExpense extends Expense
 {
-private Map(User, Double) userShares;
+private Map<User, Double> usershar;
     
     public ExactExpense(String expenseID, User paidBy, double totalAmount, Map<User, Double> userShares)
     {
     super(expenseID, paidBy, totalAmount, Expensetype.EXACT);
-    this.userShares=userShares;
+    this.usershar=userShares;
     }
  public void splitExpense()
  {
-     double totalShares=userShares.values().stream().mapToDouble(Double::doubleValue).sum();
-     if(totalShares!=getTotalAmount)
+     double totalShares=usershar.values().stream().mapToDouble(Double::doubleValue).sum();
+     if(totalShares!=getTotalAmount())
      {
          throw new IllegalArgumentException("Total shares do not equal the total amount.");
      }
      {
-         userShares.forEach((User, amount)->System.out.println(User.getUserName() +" owes "+ amount));
+         usershar.forEach((User, amount)->System.out.println(User.getUserName() +" owes "+ amount));
      }
  }
 }
